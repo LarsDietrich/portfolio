e822dac423ce6347b57a507f4f3270d64c3589f1
Joey Hoogerwerf <joeyhoogerwerf@gmail.com>
2012-05-03 13:39:32 +0200

    PHP backend to be used in image gallery.

--- php/getProject.php
+++ php/getProject.php
@@ -1,11 +1,25 @@
 <?php
 	$id_project = $_GET['id'];
 	
+	//Connect to database
 	$mysqli = new mysqli("localhost", "root", "root", "theportfolio");
 	$mysqli->select_db("theportfolio");
-	$result = $mysqli->query("SELECT descriptionMain, descriptionDetails, mainImg FROM projects WHERE id = ".$id_project);
 	
-	$project = $result->fetch_assoc();
+	//Inject queries
+	$resultset_project = $mysqli->query("SELECT descriptionMain, descriptionDetails, mainImg FROM projects WHERE id = ".$id_project);
+	$resultset_images = $mysqli->query("SELECT src FROM project_images WHERE project_id = ".$id_project);
+	
+	$project = $resultset_project->fetch_assoc();
+	
+	$images = array();
+	
+	//Fill images array
+	while($image = $resultset_images->fetch_assoc()){
+		$images[] = $image;
+	}
+	
+	//Add new key-value pair 'images' to 'project'.
+	$project["images"] = $images;
 	
 	echo json_encode($project);
 ?>
